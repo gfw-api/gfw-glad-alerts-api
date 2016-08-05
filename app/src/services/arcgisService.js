69cc0e617aa8a87171077eb582ee76cdbfeb89f9
rrequero <rareq1987@gmail.com>
2016-08-05 14:18:54 +0200

    add arcgis

---
+++ app/src/services/arcgisService.js
@@ -0,0 +1,301 @@
+'use strict';
+var logger = require('logger');
+var coRequest = require('co-request');
+var CartoDB = require('cartodb');
+var CartoDBService = require('services/cartoDBService');
+const querystring = require('querystring');
+
+const IMAGE_SERVER = 'http://gis-gfw.wri.org/arcgis/rest/services/image_services/glad_alerts_analysis/ImageServer/';
+const CONFIRMED_IMAGE_SERVER = 'http://gis-gfw.wri.org/arcgis/rest/services/image_services/glad_alerts_con_analysis/ImageServer/';
+const START_YEAR = 2015;
+const MOSAIC_RULE = {
+    'mosaicMethod': 'esriMosaicLockRaster',
+    'ascending': true,
+    'mosaicOperation': 'MT_FIRST'
+};
+
+const RASTERS = {
+    all: {
+        2015: 6,
+        2016: 4
+    },
+    confirmedOnly: {
+        2015: 7,
+        2016: 5
+    }
+};
+
+const YEAR_FOR_RASTERS = {
+    6: 2015,
+    7: 2015,
+    4: 2016,
+    5: 2016
+};
+
+class ArcgisService {
+    static generateMosaicrule(raster){
+        let mosaicrule = Object.assign({}, MOSAIC_RULE, { lockRasterIds: [raster] });
+        return mosaicrule;
+    }
+
+    static geojsonToEsriJson(geojson){
+        if(geojson.type === 'Polygon'){
+            geojson.rings = geojson.coordinates;
+            delete geojson.coordinates;
+        } else if(geojson.type === 'MultiPolygon') {
+            geojson.rings = geojson.coordinates[0];
+            delete geojson.coordinates;
+        }
+        geojson.type = 'polygon';
+        return geojson;
+    }
+
+    static getYearDay(date){
+        var start = new Date(date.getFullYear(), 0, 0);
+        var diff = date - start;
+        var oneDay = 1000 * 60 * 60 * 24;
+        var dayOfYear = Math.floor(diff / oneDay);
+        return dayOfYear;
+    }
+
+    static dateToGridCode(date){
+        return ArcgisService.getYearDay(date) + (365 * (date.getFullYear() - START_YEAR));
+    }
+
+    static rasterForDate(date, confirmed=false){
+        if(confirmed){
+            return RASTERS.confirmedOnly[date.getFullYear()];
+        } else {
+            return RASTERS.all[date.getFullYear()];
+        }
+    }
+
+    static yearForRaster(raster){
+        return YEAR_FOR_RASTERS[raster];
+    }
+
+    static rastersForPeriod(startDate, endDate, confirmed=false){
+        let rasters = [];
+
+        let begin = ArcgisService.rasterForDate(startDate, confirmed);
+        if (rasters.indexOf(begin) === -1){
+            rasters.push(begin);
+        }
+        let end = ArcgisService.rasterForDate(endDate, confirmed);
+        if (rasters.indexOf(end) === -1){
+            rasters.push(end);
+        }
+
+        return rasters;
+    }
+
+    static * getHistogram(rasters, esriJSON, confirmedOnly) {
+        let formFields = {
+            geometry: JSON.stringify(esriJSON),
+            geometryType: 'esriGeometryPolygon',
+            f: 'pjson'
+        };
+
+        let imageServer = IMAGE_SERVER;
+        if (confirmedOnly) {
+            imageServer = CONFIRMED_IMAGE_SERVER;
+        }
+
+        let results = {};
+
+        for (let i = 0, length = rasters.length; i < length; i++){
+            formFields.mosaicRule = JSON.stringify(ArcgisService.generateMosaicrule(rasters[1]));
+            logger.debug('Doing request to arcgis with url ', `${imageServer}computeHistograms`, 'and formfields');
+            let result = yield coRequest({
+                uri: `${imageServer}computeHistograms`,
+                method: 'POST',
+                form: querystring.stringify(formFields),
+                json: true
+            });
+
+            if(result.statusCode === 200) {
+                logger.debug('Response OK. body: ');
+                results[rasters[i]] = result.body;
+            } else {
+                logger.error('Error to obtain data in arcgis');
+                throw new Error('Error to obtain data in arcgis, for raster', rasters[i]);
+            }
+        }
+        logger.debug('Results', results);
+        return results;
+    }
+
+    static datesToGridCodes(begin, end, raster) {
+        let rasterYear = ArcgisService.yearForRaster(raster);
+
+        let indexes = [];
+        if(begin.getFullYear() === rasterYear) {
+            indexes.push(ArcgisService.getYearDay(begin) -1);
+        } else {
+            var dateBegin = new Date(begin.getFullYear(), 0, 1, 0,0,0);
+            indexes.push(ArcgisService.getYearDay(dateBegin) -1);
+        }
+
+        if(end.getFullYear() === rasterYear) {
+            indexes.push(ArcgisService.getYearDay(end) -1);
+        } else {
+            var dateEnd = new Date(end.getFullYear(), 11, 31, 0,0,0);
+            indexes.push(ArcgisService.getYearDay(dateEnd) -1);
+        }
+        return indexes;
+    }
+
+    static alertCount(begin, end, histograms){
+        logger.debug('Histograms', histograms);
+        let totalCount = 0;
+        if(histograms){
+            let rasters = Object.keys(histograms);
+            for (let i = 0, length = rasters.length; i < length; i++){
+                let counts = [];
+                if(histograms[rasters[i]].histograms.length > 0){
+                    counts = histograms[rasters[i]].histograms[0].counts;
+                }
+                let indexes = ArcgisService.datesToGridCodes(begin, end, rasters[i]);
+                logger.debug('counts', JSON.stringify(counts));
+                logger.debug('indexes', indexes);
+                let subCounts = counts.slice(indexes[0], indexes[1]);
+                logger.debug('subCounts', JSON.stringify(subCounts));
+                let sum = 0;
+                if(subCounts && subCounts.length > 0) {
+                    sum = subCounts.reduce(function(oldVar, newVar){
+                        return oldVar + newVar;
+                    });
+                }
+                totalCount += sum;
+            }
+        }
+        return totalCount;
+    }
+
+    static * getAlertCount(begin, end, geojson, confirmedOnly){
+        logger.info('Get alerts count with begin ', begin, ' , end', end, 'and confirmedOnly ', confirmedOnly);
+        begin = new Date(begin);
+        end = new Date(end);
+        let rasters = ArcgisService.rastersForPeriod(begin, end, confirmedOnly);
+        logger.debug('rasters', rasters);
+        try{
+            let esriJSON = ArcgisService.geojsonToEsriJson(geojson);
+            let histograms = yield ArcgisService.getHistogram(rasters, esriJSON, confirmedOnly);
+            logger.debug('histograms', histograms);
+            let alertCount = ArcgisService.alertCount(begin, end, histograms);
+            logger.debug('AlertCount', alertCount);
+            return {
+                begin: begin.toISOString(),
+                end: end.toISOString(),
+                value: alertCount,
+                notes: '' // TODO: Add notess
+            };
+        } catch(err){
+            logger.error(err);
+            throw err;
+        }
+    }
+
+    static getMaxDateFromHistograms(histograms) {
+        logger.debug('Obtaining max date from histograms');
+        let year = (Object.keys(histograms).length -1) + START_YEAR;
+        let latestHistogramKey = Math.max.apply(null, Object.keys(histograms));
+        let dayNumber = histograms[latestHistogramKey].length;
+        let resultDate = new Date(new Date(year, 0, 1, 0,0,0).getTime() + (dayNumber * 24 * 60 * 60* 1000));
+        return resultDate;
+    }
+
+    static * getFullHistogram(){
+        logger.info('Get full histogram');
+        var begin = new Date(START_YEAR, 0, 1, 0,0,0);
+        var end = new Date();
+
+        var rasters = ArcgisService.rastersForPeriod(begin, end);
+
+        var results = {};
+        for(let i = 0, length = rasters.length; i < length; i++) {
+            let url = `${IMAGE_SERVER}${rasters[i]}/info/histograms?f=pjson`;
+            logger.debug(`Doing request to ${url}`);
+            let result = yield coRequest({
+                uri: url,
+                method: 'GET',
+                headers:{
+                    'Content-type': 'application/x-www-form-urlencoded'
+                },
+                json: true
+            });
+
+            if(result.statusCode === 200) {
+                logger.debug('Response OK. body: ');
+                results[ArcgisService.yearForRaster(rasters[i])] = result.body.histograms[0].counts;
+            } else {
+                logger.error('Error to obtain data in arcgis');
+                throw new Error('Error to obtain data in arcgis, for raster', rasters[i]);
+            }
+        }
+
+        return {
+            minDate: begin.toISOString().slice(0, 10),
+            maxDate: ArcgisService.getMaxDateFromHistograms(results).toISOString().slice(0, 10),
+            counts: results
+        };
+    }
+
+    static * getAlertCountByISO(begin, end, iso, confirmedOnly){
+        logger.info('Get alerts by iso %s', iso);
+        let data = yield CartoDBService.getNational(iso);
+        if(data) {
+            logger.debug('Obtained geojson. Obtaining alerts');
+            let alerts = yield ArcgisService.getAlertCount(begin, end, data.geojson, confirmedOnly);
+            alerts.areaHa = data.areaHa;
+            return alerts;
+        }
+        return null;
+    }
+    static * getAlertCountByID1(begin, end, iso, id1, confirmedOnly){
+        logger.info('Get alerts by iso %s and id1', iso, id1);
+        let data = yield CartoDBService.getSubnational(iso, id1);
+        if(data) {
+            logger.debug('Obtained geojson. Obtaining alerts');
+            let alerts = yield ArcgisService.getAlertCount(begin, end, data.geojson, confirmedOnly);
+            alerts.areaHa = data.areaHa;
+            return alerts;
+        }
+        return null;
+    }
+    static * getAlertCountByWDPA(begin, end, wdpaid, confirmedOnly){
+        logger.info('Get alerts by wdpa %s', wdpaid);
+        let data = yield CartoDBService.getWdpa(wdpaid);
+        if(data) {
+            logger.debug('Obtained geojson. Obtaining alerts');
+            let alerts = yield ArcgisService.getAlertCount(begin, end, data.geojson, confirmedOnly);
+            alerts.areaHa = data.areaHa;
+            return alerts;
+        }
+        return null;
+    }
+    static * getAlertCountByUSE(begin, end, useTable, id, confirmedOnly){
+        logger.info('Get alerts by use %s and id', useTable, id);
+        let data = yield CartoDBService.getUse(useTable, id);
+        if(data) {
+            logger.debug('Obtained geojson. Obtaining alerts');
+            let alerts = yield ArcgisService.getAlertCount(begin, end, data.geojson, confirmedOnly);
+            alerts.areaHa = data.areaHa;
+            return alerts;
+        }
+        return null;
+    }
+    static * getAlertCountByGeostore(begin, end, geostoreHash, confirmedOnly){
+        logger.info('Get alerts by geostorehash %s', geostoreHash);
+        let data = yield CartoDBService.getGeostore(geostoreHash);
+        if(data) {
+            logger.debug('Obtained geojson. Obtaining alerts');
+            let alerts = yield ArcgisService.getAlertCount(begin, end, data.geojson, confirmedOnly);
+            alerts.areaHa = data.areaHa;
+            return alerts;
+        }
+        return null;
+    }
+}
+
+module.exports = ArcgisService;
--- app/src/services/cartoDBService.js
+++ app/src/services/cartoDBService.js
@@ -7,62 +7,30 @@ var Mustache = require('mustache');
 var NotFound = require('errors/notFound');
 var JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
 
-const WORLD = `SELECT COUNT(iso) AS value, MIN(date) as min_date, MAX(date) as max_date
-        FROM  umd_alerts_agg_analysis f
-        WHERE date >= '{{begin}}'::date
-          AND date <= '{{end}}'::date
-          AND ST_INTERSECTS(
-                ST_Transform(
-                  ST_SetSRID(
-                    ST_GeomFromGeoJSON('{{{geojson}}}'),
-                  4326),
-                3857),
-              f.the_geom_webmercator) `;
+const ISO = `SELECT ST_AsGeoJSON(the_geom) AS geojson, (ST_Area(geography(the_geom))/10000) as area_ha
+        FROM gadm2_countries_simple
+        WHERE iso = UPPER('{{iso}}')`;
 
-const ISO = `with s as (select area_ha
-            FROM gadm2_countries_simple
-            WHERE iso = UPPER('{{iso}}'))
-            SELECT COUNT(iso) AS value, MIN(date) as min_date, MAX(date) as max_date, area_ha
-                    FROM umd_alerts_agg_analysis f right join s
-                    ON iso = UPPER('{{iso}}')
-                    AND date >= '{{begin}}'::date
-                    AND date <= '{{end}}'::date
-            group by area_ha `;
+const ID1 = `SELECT ST_AsGeoJSON(the_geom) AS geojson, (ST_Area(geography(the_geom))/10000) as area_ha
+        FROM gadm2_provinces_simple
+        WHERE iso = UPPER('{{iso}}')
+          AND id_1 = {{id1}}`;
 
-const ID1 = `WITH r as (SELECT  ST_RemoveRepeatedPoints(the_geom_webmercator, 1000) the_geom_webmercator, area_ha
-                   FROM gadm2_provinces_simple
-                   WHERE iso = UPPER('{{iso}}')
-                   AND id_1 = {{id1}} )
-                SELECT COUNT(f.iso) AS value, MIN(date) as min_date, MAX(date) as max_date, area_ha
-                FROM umd_alerts_agg_analysis f INNER JOIN r ON st_intersects(r.the_geom_webmercator,f.the_geom_webmercator) and date >= '{{begin}}'::date
-                AND date <= '{{end}}'::date
-                group by area_ha
-                `;
+const WDPA = `SELECT ST_AsGeoJSON(p.the_geom) AS geojson, (ST_Area(geography(the_geom))/10000) as area_ha
+        FROM (
+          SELECT CASE
+          WHEN marine::numeric = 2 THEN NULL
+            WHEN ST_NPoints(the_geom)<=18000 THEN the_geom
+            WHEN ST_NPoints(the_geom) BETWEEN 18000 AND 50000 THEN ST_RemoveRepeatedPoints(the_geom, 0.001)
+            ELSE ST_RemoveRepeatedPoints(the_geom, 0.005)
+            END AS the_geom
+          FROM wdpa_protected_areas
+          WHERE wdpaid={{wdpaid}}
+        ) p`;
 
-const USE = `SELECT COUNT(iso) AS value, MIN(date) as min_date, MAX(date) as max_date, area_ha
-            FROM {{useTable}} u left join umd_alerts_agg_analysis f
-            on  ST_Intersects(f.the_geom_webmercator, u.the_geom_webmercator)
-            AND date >= '{{begin}}'::date
-            AND date <= '{{end}}'::date
-            WHERE u.cartodb_id = {{pid}}
-            group by area_ha`;
-
-const WDPA = `WITH p as (SELECT CASE when marine::numeric = 2 then null
-        when ST_NPoints(the_geom)<=18000 THEN the_geom_webmercator
-       WHEN ST_NPoints(the_geom) BETWEEN 18000 AND 50000 THEN ST_RemoveRepeatedPoints(the_geom_webmercator, 100)
-      ELSE ST_RemoveRepeatedPoints(the_geom_webmercator, 1000)
-       END as the_geom_webmercator, gis_area*100 as area_ha FROM wdpa_protected_areas where wdpaid={{wdpaid}})
-        SELECT COUNT(iso) AS value, MIN(date) as min_date, MAX(date) as max_date, area_ha
-        FROM umd_alerts_agg_analysis f right join p
-        on ST_Intersects(f.the_geom_webmercator, p.the_geom_webmercator)
-              AND date >= '{{begin}}'::date
-              AND date <= '{{end}}'::date
-            group by area_ha`;
-
-const LATEST = `SELECT DISTINCT date
-        FROM umd_alerts_agg_analysis
-        ORDER BY date DESC
-        LIMIT {{limit}}`;
+const USE = `SELECT ST_AsGeoJSON(the_geom) AS geojson, (ST_Area(geography(the_geom))/10000) as area_ha
+        FROM {{useTable}}
+        WHERE cartodb_id = {{pid}}`;
 
 
 
@@ -84,123 +52,71 @@ var deserializer = function(obj) {
 };
 
 
-let getToday = function() {
-    let today = new Date();
-    return `${today.getFullYear().toString()}-${(today.getMonth()+1).toString()}-${today.getDate().toString()}`;
-};
-
-let getYesterday = function() {
-    let yesterday = new Date(Date.now() - (24 * 60 * 60 * 1000));
-    return `${yesterday.getFullYear().toString()}-${(yesterday.getMonth()+1).toString()}-${yesterday.getDate().toString()}`;
-};
-
-
-let defaultDate = function() {
-    let to = getToday();
-    let from = getYesterday();
-    return from + ',' + to;
-};
-
 class CartoDBService {
 
     constructor() {
         this.client = new CartoDB.SQL({
             user: config.get('cartoDB.user')
         });
-        this.apiUrl = config.get('cartoDB.apiUrl');
     }
 
-    getDownloadUrls(query, params) {
-        try{
-            let formats = ['csv', 'geojson', 'kml', 'shp', 'svg'];
-            let download = {};
-            let queryFinal = Mustache.render(query, params);
-            queryFinal = queryFinal.replace('COUNT(iso) AS value, MIN(date) as min_date, MAX(date) as max_date', ' f.date, st_transform(f.the_geom_webmercator, 4326) as the_geom, ST_Y(st_transform(the_geom_webmercator, 4326)) as lat, ST_X(st_transform(the_geom_webmercator, 4326)) as long');
-            queryFinal = encodeURIComponent(queryFinal);
-            for(let i=0, length = formats.length; i < length; i++){
-                download[formats[i]] = this.apiUrl + '?q=' + queryFinal + '&format=' + formats[i];
-            }
-            return download;
-        }catch(err){
-            logger.error(err);
-            throw err;
-        }
-    }
-
-
-    * getNational(iso, period = defaultDate()) {
+    * getNational(iso, period) {
         logger.debug('Obtaining national of iso %s', iso);
-        let periods = period.split(',');
         let params = {
-            iso: iso,
-            begin: periods[0],
-            end: periods[1]
+            iso: iso
         };
 
         let data = yield executeThunk(this.client, ISO, params);
         if (data.rows && data.rows.length > 0) {
             let result = data.rows[0];
-
-            result.downloadUrls = this.getDownloadUrls(ISO, params);
-            return result;
+            return {geojson: JSON.parse(result.geojson), areaHa: result.area_ha};
         }
         return null;
     }
 
-    * getSubnational(iso, id1, period = defaultDate()) {
+    * getSubnational(iso, id1, period) {
         logger.debug('Obtaining subnational of iso %s and id1', iso, id1);
-        let periods = period.split(',');
         let params = {
             iso: iso,
-            id1: id1,
-            begin: periods[0],
-            end: periods[1]
+            id1: id1
         };
 
         let data = yield executeThunk(this.client, ID1, params);
         if (data.rows && data.rows.length > 0) {
             let result = data.rows[0];
-
-            result.downloadUrls = this.getDownloadUrls(ID1, params);
-            return result;
+            return {geojson: JSON.parse(result.geojson), areaHa: result.area_ha};
         }
         return null;
     }
 
-    * getUse(useTable, id, period = defaultDate()) {
+    * getUse(useTable, id, period) {
         logger.debug('Obtaining use with id %s', id);
-        let periods = period.split(',');
+
         let params = {
             useTable: useTable,
-            pid: id,
-            begin: periods[0],
-            end: periods[1]
+            pid: id
         };
 
         let data = yield executeThunk(this.client, USE, params);
 
         if (data.rows && data.rows.length > 0) {
             let result = data.rows[0];
-            result.downloadUrls = this.getDownloadUrls(USE, params);
-            return result;
+            return {geojson: JSON.parse(result.geojson), areaHa: result.area_ha};
         }
         return null;
     }
 
-    * getWdpa(wdpaid, period = defaultDate()) {
+    * getWdpa(wdpaid, period) {
         logger.debug('Obtaining wpda of id %s', wdpaid);
-        let periods = period.split(',');
+
         let params = {
-            wdpaid: wdpaid,
-            begin: periods[0],
-            end: periods[1]
+            wdpaid: wdpaid
         };
 
         let data = yield executeThunk(this.client, WDPA, params);
         if (data.rows && data.rows.length > 0) {
             let result = data.rows[0];
-            result.downloadUrls = this.getDownloadUrls(WDPA, params);
-            return result;
+            return {geojson: JSON.parse(result.geojson), areaHa: result.area_ha};
         }
         return null;
     }
@@ -217,47 +133,13 @@ class CartoDBService {
             console.error(result);
             return null;
         }
-        return yield deserializer(result.body);
-    }
-
-    * getWorld(hashGeoStore, period = defaultDate()) {
-        logger.debug('Obtaining world with hashGeoStore %s', hashGeoStore);
-
-        let geostore = yield this.getGeostore(hashGeoStore);
-        if (geostore && geostore.geojson) {
-            logger.debug('Executing query in cartodb with geostore', geostore);
-            let periods = period.split(',');
-            let params = {
-                geojson: JSON.stringify(geostore.geojson.features[0].geometry),
-                begin: periods[0],
-                end: periods[1]
-            };
-
-            let data = yield executeThunk(this.client, WORLD, params);
-            if (data.rows && data.rows.length > 0) {
-                let result = data.rows[0];
-                result.area_ha = geostore.areaHa;
-                result.downloadUrls = this.getDownloadUrls(WORLD, params);
-                return result;
-            }
-            return null;
-        }
-        throw new NotFound('Geostore not found');
-    }
-
-    * latest(limit=3) {
-        logger.debug('Obtaining latest with limit %s', limit);
-        let params = {
-            limit: limit
-        };
-        let data = yield executeThunk(this.client, LATEST, params);
-        logger.debug('data', data);
-        if (data.rows ) {
-            let result = data.rows;
-            return result;
+        let geostore = yield deserializer(result.body);
+        if (geostore) {
+            return geostore;
         }
         return null;
     }
+
 
 }
 
